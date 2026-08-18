f2a51da62475414ebb42ff0c045c855fe016bf56
Vansh192302 <VANSH.33710@stu.upes.ac.in>
2026-08-18 21:44:44 +0530

    Add files via upload

---
+++ Day2code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum, diff, product, quotient;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+    diff = a - b;
+    product = a * b;
+    quotient = a / b;
+
+    printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d", sum, diff, product, quotient);
+
+    return 0;
+}
