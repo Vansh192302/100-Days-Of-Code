f8c442f4f63e471a28f536733d558f61dd597727
Vansh192302 <VANSH.33710@stu.upes.ac.in>
2026-08-23 21:09:32 +0530

    Add files via upload

---
+++ day5_5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int seconds, hours, minutes, sec;
+    scanf("%d", &seconds);
+    hours = seconds / 3600;
+    seconds = seconds % 3600;
+    minutes = seconds / 60;
+    sec = seconds % 60;
+    printf("%d:%d:%d", hours, minutes, sec);
+    return 0;
+}
