486577e7925a94b518c699fe884f91b7b1b8b090
Vansh Bajaj <VANSH.33710@stu.upes.ac.in>
2026-09-21 15:45:48 +0530

    Completed Day 17_17

---
+++ DAY17_17.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, count = 0;
+
+    scanf("%d", &n);
+
+    if(n <= 1)
+    {
+        printf("Not Prime");
+        return 0;
+    }
+
+    for(i = 1; i <= n; i++)
+    {
+        if(n % i == 0)
+            count++;
+    }
+
+    if(count == 2)
+        printf("Prime");
+    else
+        printf("Not Prime");
+
+    return 0;
+}
